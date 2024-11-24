ff310a680d1ca49e287f9c34f08b1ef03e5feef9
ivan-nesterenko <ivan.nesterenko@ukr.net>
2024-11-24 19:17:00 +0200

    added drizzle dep

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import * as schema from '../drizzle/schema';
+import { DrizzleAsyncProvider } from '../drizzle/drizzle.provider';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(DrizzleAsyncProvider)
+    private db: NodePgDatabase<typeof schema>
+  ) {}
+
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async getFirstRole() {
+    const existingUserRole = await this.db.query.user_role.findFirst({});
+    return existingUserRole;
   }
 }
