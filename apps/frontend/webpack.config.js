7c8bf7aaab8c38689b668a4eaf780ad342e616ce
Bohdan Palii <bohdan.palii@uapp-llc.com>
2025-07-06 22:32:19 +0200

    Enhance webpack configuration with path resolution and update output settings

--- apps/frontend/webpack.config.js
+++ apps/frontend/webpack.config.js
@@ -1,35 +1,44 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
-const { join } = require('path');
+/* eslint-disable */
+
+const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
+const { NxReactWebpackPlugin } = require("@nx/react/webpack-plugin");
+const { join, resolve } = require("path");
 
 module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/frontend'),
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    alias: {
+      "@/ui-shared": resolve(__dirname, "../../libs/ui-shared/src"),
+      "@": resolve(__dirname, "src"),
+    },
   },
+
+  output: {
+    path: join(__dirname, "../../dist/apps/frontend"),
+  },
+
   devServer: {
     port: 3000,
     historyApiFallback: {
-      index: '/index.html',
-      disableDotRule: true,
-      htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'],
+      index: "/index.html",
+      htmlAcceptHeaders: ["text/html", "application/xhtml+xml"],
     },
   },
+
   plugins: [
     new NxAppWebpackPlugin({
-      tsConfig: './tsconfig.app.json',
-      compiler: 'babel',
-      main: './src/main.tsx',
-      index: './src/index.html',
-      baseHref: '/',
-      assets: ['./src/favicon.ico', './src/assets'],
-      styles: ['./src/styles.css'],
-      outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
-      optimization: process.env['NODE_ENV'] === 'production',
+      tsConfig: "./tsconfig.app.json",
+      compiler: "babel",
+      main: "./src/index.tsx",
+      index: "./public/index.html",
+      baseHref: "/",
+      assets: ["./public/favicon.ico", "./src/assets"],
+      styles: ["./public/styles.css"],
+      outputHashing: process.env["NODE_ENV"] === "production" ? "all" : "none",
+      optimization: process.env["NODE_ENV"] === "production",
     }),
     new NxReactWebpackPlugin({
-      // Uncomment this line if you don't want to use SVGR
-      // See: https://react-svgr.com/
-      // svgr: false
+      // svgr: false,
     }),
   ],
 };
